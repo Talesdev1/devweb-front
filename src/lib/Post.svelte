0ac9eb82757ab6ffb211f5b31d0e4d5967eb4bc4
Talesdev1 <talesdeveloper1@gmail.com>
2025-09-03 14:49:19 +0000

    add `Post.svelte`

---
+++ src/lib/Post.svelte
@@ -0,0 +1,27 @@
+<script>
+    let { title, url } = $props()
+</script>
+
+<article>
+    <h1><a href={url}>{title}</a></h1>
+</article>
+
+<style>
+    h1 {
+        margin: 0;
+        font-size: 1.35rem;
+        line-height: 1.1rem;
+        margin-bottom: 12px;
+        color: #1e1e1e;
+    }
+
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+
+    a:hover {
+        text-decoration: underline;
+        color: #A31D1D;
+    }
+</style>
