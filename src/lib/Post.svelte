8bb05d336dfba9869e53f9c5a4577355a3083c04
Talesrib <talespiano@gmail.com>
2025-09-10 09:08:22 -0300

    post component

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -1,27 +1,64 @@
 <script>
-    let { title, url } = $props()
+    let { title, url, summary, created_at, updated_at, tags } = $props()
 </script>
 
 <article>
     <h1><a href={url}>{title}</a></h1>
+    <p>{summary}</p>
+    <p class="info">{created_at} [{updated_at}]</p>
+    {#if tags.length > 0}
+    <ul class="info">
+      {#each tags as tag}
+      <li><a href={`/tags/${tag}`}>#{tag}</a></li>
+      {/each}
+    </ul>
+    {/if}
 </article>
 
 <style>
+  article {
+    padding: 18px;
+    border:1px solid #2e2e33;
+    color: #1e1e1e;
+  }
+
     h1 {
         margin: 0;
         font-size: 1.35rem;
         line-height: 1.1rem;
         margin-bottom: 12px;
-        color: #1e1e1e;
     }
 
     a {
         text-decoration: none;
         color: inherit;
+        cursor: pointer;
     }
 
     a:hover {
         text-decoration: underline;
         color: #A31D1D;
+    }
+
+    p {
+      font-size: calc(18rem/16);
+      margin-bottom: 12px;
+    }
+
+    .info {
+      color: gray;
+      font-size: calc(10rem/16);
+      margin: 0;
+    }
+
+    ul {
+      display: flex;
+      justify-content: flex-start;
+      padding-left: 0;
+    }
+
+    li {
+      list-style: none;
+      margin-right: 12px;
     }
 </style>
