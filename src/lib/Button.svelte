118fa5cc17d80b07086fc97f876d19dc89a22e89
Talesrib <talespiano@gmail.com>
2025-09-21 22:56:12 -0300

    add tipos confirmar e cancelar em componente `Button`

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -2,17 +2,35 @@
   let { type, content } = $props()
 </script>
 
-<button>{content}</button>
+<button class={type === 'confirm' ? "confirm" : "cancel"}>{content}</button>
 
 
 <style>
-  button {
+  .confirm {
     background-color: green;
     color: white;
+  }
+
+  .cancel {
+    background-color: #f9fafb;
+    color: gray;
+  }
+
+  button {
     font-weight: 600;
-    width: 100%;
-    padding: 10px;
+    /* width: 100%; */
+    padding: 10px 20px;
     border-radius: 8px;
     border: 0;
+  }
+
+  button:hover.confirm {
+    background-color: rgba(0, 128, 0, 0.904);
+    cursor: pointer;
+  }
+
+  button:hover.cancel {
+    background-color: #e2e2e2;
+    cursor: pointer;
   }
 </style>
